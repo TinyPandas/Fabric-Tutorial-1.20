e4e1c96e8fd05a22d2a2beb3e421fd760120a4e9
TinyPandas <chris.barwick16@gmail.com>
2023-06-12 13:41:29 -0700

    Tooltips!

--- src/main/java/net/panda/tutorialmod/block/custom/VirusBlock.java
+++ src/main/java/net/panda/tutorialmod/block/custom/VirusBlock.java
@@ -3,15 +3,25 @@ package net.panda.tutorialmod.block.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+import static net.panda.tutorialmod.Util.translationKey;
 
 public class VirusBlock extends Block {
 
@@ -38,6 +48,12 @@ public class VirusBlock extends Block {
             }
         }
         super.precipitationTick(state, world, pos, precipitation);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable BlockView world, List<Text> tooltip, TooltipContext options) {
+        super.appendTooltip(stack, world, tooltip, options);
+        tooltip.add(Text.translatable(translationKey("tooltip", "virus_block")).formatted(Formatting.DARK_GREEN));
     }
 
     private BlockPos getRandomBlockPosOffset(BlockPos pos) {
--- src/main/java/net/panda/tutorialmod/item/custom/CustomWandItem.java
+++ src/main/java/net/panda/tutorialmod/item/custom/CustomWandItem.java
@@ -1,17 +1,23 @@
 package net.panda.tutorialmod.item.custom;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import net.panda.tutorialmod.Util;
 import net.panda.tutorialmod.block.ModBlocks;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class CustomWandItem extends Item {
 
@@ -43,6 +49,12 @@ public class CustomWandItem extends Item {
         }
 
         return super.use(world, user, hand);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        super.appendTooltip(stack, world, tooltip, context);
+        tooltip.add(Text.translatable(Util.translationKey("tooltip", "custom_wand_item")).formatted(Formatting.AQUA));
     }
 
     private void outputRandomNumber(PlayerEntity player) {
