a1e547d081fac544887481f7be70413e230b8174
TinyPandas <chris.barwick16@gmail.com>
2023-06-12 12:50:06 -0700

    Custom Wand + useOnBlock and use

--- src/main/java/net/panda/tutorialmod/item/ModItems.java
+++ src/main/java/net/panda/tutorialmod/item/ModItems.java
@@ -4,13 +4,12 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
-import net.minecraft.util.Identifier;
 import net.panda.tutorialmod.ExampleMod;
+import net.panda.tutorialmod.item.custom.CustomWandItem;
 
 import static net.panda.tutorialmod.Util.id;
 
@@ -21,6 +20,10 @@ public class ModItems {
             ModItemGroup.registryKey());
     public static final Item FILLED_CUSTOM_ITEM = registerItem("filled_custom_item",
             new Item(new FabricItemSettings()),
+            ModItemGroup.registryKey());
+
+    public static final Item CUSTOM_WAND_ITEM = registerItem("custom_wand_item",
+            new CustomWandItem(new FabricItemSettings().maxCount(1)),
             ModItemGroup.registryKey());
 
     private static Item registerItem(String name, Item item, RegistryKey<ItemGroup> itemGroup) {
